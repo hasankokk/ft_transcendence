6ff284c529e6f1b2d59a2fdc41009c638cdc8acf
timurci <timurcakmakoglu@gmail.com>
2024-03-31 10:53:06 +0300

    improved chat/pong network communication log

--- src/user-web/ft_transcendence/static/chat/socket_example.js
+++ src/user-web/ft_transcendence/static/chat/socket_example.js
@@ -43,9 +43,23 @@ function chatRoom() {
 
     chatSocket.onmessage = function (e) {
       const data = JSON.parse(e.data);
-      if ("type" in data & data.type === "pong.status") {
-        const info = JSON.stringify(JSON.parse(data.message), null, 2);
-        document.querySelector("#ping-log").value = info + "\n";
+      if (("type" in data) & (data.type === "pong.status")) {
+        const info = JSON.parse(data.message);
+        document.querySelector("#ping-log").value = JSON.stringify(
+          info,
+          null,
+          4
+        );
+        if (("current_players" in info) & (info.current_players.length > 0)) {
+          replaceOutput(info.players[info.current_players[0]], "player1-log", [
+            "0",
+            info.current_players[0],
+          ]);
+          replaceOutput(info.players[info.current_players[1]], "player2-log", [
+            "1",
+            info.current_players[1],
+          ]);
+        }
       } else {
         document.querySelector("#chat-log").value += data.message + "\n";
       }
@@ -89,7 +103,7 @@ function chatRoom() {
 
   document.querySelector("#room-ready").onclick = function (e) {
     pingReady(chatSocket);
-  }
+  };
 }
 
 function pingPong(socket) {
@@ -108,4 +122,41 @@ function pingReady(socket) {
       message: "",
     })
   );
+}
+
+function syntaxHighlight(json) {
+  if (typeof json != "string") {
+    json = JSON.stringify(json, undefined, 4);
+  }
+  json = json
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+  return json.replace(
+    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
+    function (match) {
+      let cls = "number";
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
+          cls = "key";
+        } else {
+          cls = "string";
+        }
+      } else if (/true|false/.test(match)) {
+        cls = "boolean";
+      } else if (/null/.test(match)) {
+        cls = "null";
+      }
+      return '<span class="' + cls + '">' + match + "</span>";
+    }
+  );
+}
+
+function replaceOutput(input, targetElementId, playerInfo = ["x", "username"]) {
+  const target = document.getElementById(targetElementId);
+  target.innerHTML = "";
+  target.appendChild(document.createElement("h3")).innerHTML =
+    "player " + playerInfo[0] + ": " + playerInfo[1];
+  target.appendChild(document.createElement("pre")).innerHTML =
+    syntaxHighlight(input);
 }
